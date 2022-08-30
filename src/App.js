79689362cb145394e13c36774ec8aa6d2a89b0e6
Rafael Soares <r.soares2015@outlook.com>
2022-08-30 14:44:37 -0300

    iniciando o projeto trybetunes 🎶

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <section>
+        <h1>TrybeTunes</h1>
+      </section>
+    );
   }
 }
 
