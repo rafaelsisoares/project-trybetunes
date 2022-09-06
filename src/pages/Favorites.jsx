e4397211679670be4221a0b9a3c9e28ac7a086b4
Rafael Soares <r.soares2015@outlook.com>
2022-09-06 01:22:17 -0300

    Começando o décimo terceiro requisito

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class Favorites extends React.Component {
+  state = {
+    isLoading: true,
+    favoriteSongs: [],
+  };
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  getFavorites = async () => {
+    const songs = await getFavoriteSongs();
+    this.setState({
+      favoriteSongs: songs,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { isLoading, favoriteSongs } = this.state;
     return (
       <section>
-        <p>Favorites</p>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          favoriteSongs.map(({ trackId, trackName, previewUrl }) => (
+            <div key={ trackId }>
+              <MusicCard
+                trackId={ trackId }
+                trackName={ trackName }
+                previewUrl={ previewUrl }
+              />
+            </div>
+          ))
+        )}
       </section>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  state = {
+    isLoading: true,
+    userName: '',
+    userEmail: '',
+    userDescription: '',
+    userImage: '',
+  };
+
+  componentDidMount() {
+    this.renderUserInfo();
+  }
+
+  renderUserInfo = async () => {
+    const { name, email, description, image } = await getUser();
+    this.setState({
+      userName: name,
+      userEmail: email,
+      userDescription: description,
+      userImage: image,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { isLoading, userName, userDescription, userEmail, userImage } = this.state;
     return (
       <section>
-        <p>Profile</p>
+        {
+          isLoading ? <Loading />
+            : (
+              <div>
+                <img src={ userImage } alt={ userName } data-testid="profile-image" />
+                <p>{userName}</p>
+                <p>{userEmail}</p>
+                <p>{userDescription}</p>
+                <Link to="/profile/edit">Editar perfil</Link>
+              </div>
+            )
+        }
       </section>
     );
   }
